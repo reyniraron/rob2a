662cfe0742c8d60418a0492b7c5c5011818f0dec
Eiríkur Benediktsson <ebe@tskoli.is>
2019-09-05 12:58:27 +0000

    setti inn lýsingu á tilgangi headers

--- headers/myheader.h
+++ headers/myheader.h
@@ -1,4 +1,7 @@
-
+/*
+The head file stores all constants and declarations of functions
+note that you have to find out your own constants these are not correct
+*/
 #define BASETIME 1500
 #define FULLPOWER 127
 
