f784d20e2974e0a6550d89bf401a4ece36467723
Eiríkur Benediktsson <ebe@tskoli.is>
2019-09-06 14:09:57 +0000

    breytti skilgr

--- includes/headers/myheader.h
+++ includes/headers/myheader.h
@@ -2,6 +2,9 @@
 The head file stores all constants and declarations of functions
 note that you have to find out your own constants these are not correct
 */
+#ifndef MYHEADER_H_
+#define MYHEADER_H_
+#endif
 #define BASETIME 1500
 #define FULLPOWER 127
 
