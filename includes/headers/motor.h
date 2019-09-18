5ba322acb4628057bc5337cc99b6e5eefeda348f
Victor-733 <victorwahid@gmail.com>
2019-09-18 12:02:32 +0000

    Add code for part 2

--- includes/headers/motor.h
+++ includes/headers/motor.h
@@ -3,7 +3,9 @@
 #endif
 #define FULL_POWER 127
 #define BASE_TIME 1000
+#define BASE_DIST 430
 
 void driveFor(int time, bool direction);
 void stopMotors();
 void resetEncoders();
+void driveForDistance();
