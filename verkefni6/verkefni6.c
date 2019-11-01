cd04856c3f8c086e0dccbb7fd90826fb0dd43a26
Victor-733 <victorwahid@gmail.com>
2019-11-01 14:55:14 +0000

    Initial implementation of findLine function
    Co-authored-by: Reynir Aron <2250637+reyniraron@users.noreply.github.com>

--- verkefni6/verkefni6.c
+++ verkefni6/verkefni6.c
@@ -20,13 +20,47 @@
 #define POWER 63
 #define LOW_POWER POWER / 4
 
+void findLine(int sensorNo, int threshold, int fullPower, int lowPower) {
+	// -1 = left
+  // 0 = center
+  // 1 = right
+
+	// If left saw the line last, steer left
+	if (sensorNo == -1) {
+		// writeDebugStreamLine("Turning left");
+
+		if (SensorValue(lineFollowerLEFT) < threshold) {
+			motor[leftMotor] = lowPower;
+			motor[rightMotor] = fullPower;
+		}
+	}
+	// If right saw the line last, steer right
+	else if (sensorNo == 1) {
+		// writeDebugStreamLine("Turning right");
+
+		if (SensorValue(lineFollowerRIGHT) < threshold) {
+			motor[leftMotor] = fullPower;
+			motor[rightMotor] = lowPower;
+		}
+	}
+	// If center saw the line last, the function should not have been called!
+	else {
+		// writeDebugStreamLine("Why did this function get called?");
+
+		motor[leftMotor] = fullPower;
+		motor[rightMotor] = fullPower;
+	}
+}
+
 //+++++++++++++++++++++++++++++++++++++++++++++| MAIN |+++++++++++++++++++++++++++++++++++++++++++++++
 task main()
 {
   StartTask(watchForStop);  // Watch for stop button presses
   wait1Msec(2000);
 
-  int threshold = 2300;  // Found by adding sensor values for dark and light together and dividing by 2
+  int maxSensorNo;
+
+  int threshold = 2340;  // Found by adding sensor values for dark and light together and dividing by 2
   while (true) {
   	// Show sensor values on LCD
     // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -+
@@ -39,20 +73,52 @@ task main()
     displayNextLCDNumber(SensorValue(lineFollowerRIGHT));   //  x  x  x   |
     // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -+
 
+    int leftValue = SensorValue(lineFollowerLEFT);
+    int centerValue = SensorValue(lineFollowerCENTER);
+    int rightValue = SensorValue(lineFollowerRIGHT);
+
+    bool isOnLine = false;
+
+    // writeDebugStreamLine("Max sensor: %d", maxSensorNo);
+
     // If right sensor sees dark, counter-steer right
-    if (SensorValue(lineFollowerRIGHT) > threshold) {
+    if (rightValue > threshold) {
       motor[leftMotor]  = POWER;
       motor[rightMotor] = LOW_POWER;
+
+      // Right has highest value
+      if (rightValue > centerValue && rightValue > leftValue) {
+	    	maxSensorNo = 1;
+	    }
+	    isOnLine = true;
     }
     // If center sensor sees dark, go straight
-    if (SensorValue(lineFollowerCENTER) > threshold) {
+    if (centerValue > threshold) {
       motor[leftMotor]  = POWER;
       motor[rightMotor] = POWER;
+
+      // Center has highest value
+      if (centerValue > leftValue && centerValue > rightValue) {
+    		maxSensorNo = 0;
+    	}
+    	isOnLine = true;
     }
     // If left sensor sees dark, counter-steer left
-    if (SensorValue(lineFollowerLEFT) > threshold) {
+    if (leftValue > threshold) {
       motor[leftMotor]  = LOW_POWER;
       motor[rightMotor] = POWER;
+
+      // Left has highest value
+	    if (leftValue > centerValue && leftValue > rightValue) {
+	    	maxSensorNo = -1;
+	    }
+	    isOnLine = true;
+    }
+
+    // If robot is not on line, find line
+    if (!isOnLine) {
+    	// writeDebugStreamLine("Robot lost line!");
+    	findLine(maxSensorNo, threshold, POWER, LOW_POWER);
     }
   }
 }
