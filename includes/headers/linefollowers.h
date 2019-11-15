2dc740ae56f1a703530304548e01429bf87c44db
Victor-733 <victorwahid@gmail.com>
2019-11-15 15:00:04 +0000

    Reorder include
    Co-authored-by: Reynir Aron <2250637+reyniraron@users.noreply.github.com>

--- includes/headers/linefollowers.h
+++ includes/headers/linefollowers.h
@@ -1,7 +1,7 @@
-#include "encoders.h"
-
 #ifndef LINEFOLLOWER_H
 #define LINEFOLLOWER_H
+
+#include "encoders.h"
 
 void followLineForDistance(int distance, bool direction, int normalSpeed, int slowerSpeed);
 
