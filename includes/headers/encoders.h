4f007e9ad10e5149100c4ce6c81a935adb0cac47
Reynir Aron <reyniraron@gmail.com>
2019-12-04 11:01:00 +0000

    Move constants into header definition

--- includes/headers/encoders.h
+++ includes/headers/encoders.h
@@ -1,8 +1,8 @@
-#define BASE_DIST 430
-#define BASE_TURN 930
-
 #ifndef MOTOR_H_
 #define MOTOR_H_
+
+#define BASE_DIST 430
+#define BASE_TURN 930
 
 void resetEncoders();
 void driveForDistance(int distance, bool direction, int normalSpeed, int slowerSpeed);
